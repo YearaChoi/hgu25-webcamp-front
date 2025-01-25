4b9c7c6524cee839ddf054415b096c63175a4f8d
YearaChoi <22200750@handong.ac.kr>
2025-01-26 01:38:14 +0900

    feat: 페이지 이동시 scroll to top 적용

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import MainPage from "./pages/MainPage";
 import PostDetailPage from "./pages/PostDetailPage";
 import PostListPage from "./pages/PostListPage";
 import PostCreatePage from "./pages/PostCreatePage";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<MainPage />}></Route>
         <Route path="/post/create" element={<PostCreatePage />}></Route>
